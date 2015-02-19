4e9f3dc0ce426ad47f015b78b873ae43778247fb
James Warren <jwarren116@gmail.com>
2015-02-19 11:28:57 -0800

    Add AJAX and jQuery to post/edit without reloading in journal.js and templates

--- static/journal.js
+++ static/journal.js
@@ -1,21 +1,87 @@
-$( function() {
-  $('#editBtn').click( function() {
-    $('.detailForm').hide('slow', function() {
-      $('.editForm').show('slow');
+$( document ).ready( function() {
+
+  $("#addBtn").click( function( event ) {
+    event.preventDefault();
+    add_post();
+  });
+
+  function add_post() {
+    $.ajax({
+      url: "/add",
+      type: "POST",
+      dataType: 'json',
+      data: { "title": $("#title").val(), 'text': $("#text").val()},
+      success: success
+    });
+  }
+
+  function success( response ) {
+    $("#addBtn").trigger('reset');
+    var template = "<article id='entry={{id}}'>"+
+                    "<a href='/detail/{{id}}'><h1 class='headingLink'>{{title}}</h1></a>"+
+                    "<p><small>{{created}}</small>"+
+                    "<div>{{{text}}}</br>"+
+                    "<hr class='titleDivider'></div>"+
+                    "</article>";
+    var rendered = Mustache.render(template, response);
+    $(".createForm").after(rendered);
+    $(".createForm").hide('slow');
+  }
+
+
+
+$("#submitBtn").click( function( event ) {
+    update_post();
+  });
+
+  function update_post() {
+    var id = $("article").attr("id")
+    $.ajax({
+      url: "/edit",
+      type: "POST",
+      dataType: 'json',
+      data: { "id": id, "title": $("#title").val(), "text": $("#text").val()},
+      success: upd_success
+    });
+  }
+  function upd_success( response ) {
+    $("#submitBtn").trigger('reset');
+    var template = "<div class='detailForm'>"+
+                    "<article id='{{id}}'>"+
+                    "<h1 class='headingLink'>{{title}}</h1>"+
+                    "<p><small>{{created}}</small></p>"+
+                    "<div>{{{text}}}</br></br>"+
+                    "<hr class='titleDivider'>"+
+                    "</div></article></div>";
+    var updated = Mustache.render(template, response);
+    $(".editForm").after(updated);
+    $(".editForm").hide('slow');
+  }
+
+
+
+
+
+  $( function() {
+    $('#editBtn').click( function() {
+      $('.detailForm').hide('slow', function() {
+        $('.editForm').show('slow');
+      });
     });
   });
-});
 
-$( function() {
-  $('#cancelBtn').click( function() {
-    $('.editForm').hide('slow', function() {
-      $('.detailForm').show('slow');
+  $( function() {
+    $('#cancelBtn').click( function() {
+      $('.editForm').hide('slow', function() {
+        $('.detailForm').show('slow');
+      });
     });
   });
-});
 
-$( function() {
-  $('#createBtn').click( function() {
-    $('.createForm').show('slow');
+  $( function() {
+    $('#createBtn').click( function() {
+      $('.createForm').show('slow');
+    });
   });
+
 });
